5b83bef79a25dfd95d1ae3e3285ff337223deffc
IGMA-IGMA <i9296517650@yandex.ru>
2026-08-05 18:47:18 +0300

    test redis connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("hello")
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	ctx := context.Background()
+
+	ok, err := rdb.SetNX(ctx, "test:lock", "1", 10*time.Second).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("lock acquired:", ok)
 }
