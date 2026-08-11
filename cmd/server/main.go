259b512d99c5c41ba7cc279ba75bb2f81bc21f01
IGMA-IGMA <i9296517650@yandex.ru>
2026-08-11 11:50:52 +0300

    update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,3 +22,38 @@ func main() {
 
 	fmt.Println("lock acquired:", ok)
 }
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/IGMA-IGMA/go-distributed-lock/internal/database"
+	"github.com/IGMA-IGMA/go-distributed-lock/internal/handler"
+	"github.com/IGMA-IGMA/go-distributed-lock/internal/model"
+	"github.com/IGMA-IGMA/go-distributed-lock/internal/repository"
+	"github.com/IGMA-IGMA/go-distributed-lock/internal/service"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	db, err := database.NewPostgres()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&model.Product{})
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	repo := repository.NewProductRepository(db)
+	svc := service.NewProductService(repo, rdb)
+	h := handler.NewProductHandler(svc)
+
+	http.HandleFunc("/products/update-quantity", h.UpdateQuantity)
+
+	log.Println("server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
